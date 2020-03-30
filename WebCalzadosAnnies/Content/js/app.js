bd70b3a4323d6f9e35c5c5b5dde10fc04e07a838
egvelasquez_vector <egvelasquez@vectoritcgroup.com>
2020-03-30 10:01:30 -0500

    modificacion app.js

--- WebCalzadosAnnies/Content/js/app.js
+++ WebCalzadosAnnies/Content/js/app.js
@@ -41,7 +41,7 @@
         };
         var DataTableLengthChange = false;
         var DataTablePageLength = 20;
-        var TablasPageLength = 50;
+        var TablasPageLength = 100;
         // Configuracion general select2
         var Select2AllowClear = true;
         var Select2Style = {
